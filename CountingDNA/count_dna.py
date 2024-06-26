a94a7fa597c64a3a167ab7046d5fe198e238c24c
Daniel Nguyen <53669174+DanielNguyener@users.noreply.github.com>
2024-06-25 21:52:56 -0500

    added comments

--- CountingDNA/count_dna.py
+++ CountingDNA/count_dna.py
@@ -6,7 +6,7 @@
 # py start.py (bash)
 
 def count_nucleotides(dna_string):
-    # Count occurrences of each nucleotide
+
     count_A = dna_string.count('A')
     count_C = dna_string.count('C')
     count_G = dna_string.count('G')
@@ -20,14 +20,12 @@ def read_dna_from_file(file_path):
         dna_string = file.readline().strip()  # Read only the first line and strip any whitespace
     return dna_string
 
-# Specify the path to your input file
 file_path = 'rosalind_dna.txt'
 
 # Read the DNA string from the file
 dna_string = read_dna_from_file(file_path)
 
-# Get the counts
 counts = count_nucleotides(dna_string)
 
-# Print the results separated by spaces
+# Print results separated by spaces
 print(' '.join(map(str, counts)))
